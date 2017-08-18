e50e6aeee3d6e378b93427c4ae89e4bad266fc2e
Shuyun <shuyungogogo@hotmail.com>
2017-08-18 11:17:30 +0800

    commit

---
+++ app/src/main/java/shuyun/PhantomView/Utils/PhantomColor.java
@@ -0,0 +1,28 @@
+package shuyun.PhantomView.Utils;
+
+/**
+ * Created by Shuyun on 2017/8/18 0018.
+ */
+public class PhantomColor {
+
+    public static final int Red_500 = 0xFFe51c23;
+    public static final int Pink_500 = 0xFF9c27b0;
+    public static final int Purple_500 = 0xFFe51c23;
+    public static final int DeepPurple_500 = 0xFF673ab7;
+    public static final int Indigo_500 = 0xFF3f51b5;
+    public static final int Blue_500 = 0xFF5677fc;
+    public static final int LightBlue_500 = 0xFF03a9f4;
+    public static final int Cyan_500 = 0xFF00bcd4;
+    public static final int Teal_500 = 0xFF009688;
+    public static final int Green_500 = 0xFF259b24;
+    public static final int LightGreen_500 = 0xFF8bc34a;
+    public static final int Lime_500 = 0xFFcddc39;
+    public static final int Yellow_500 = 0xFFffeb3b;
+    public static final int Amber_500 = 0xFFffc107;
+    public static final int Orange_500 = 0xFFff9800;
+    public static final int DeepOrange_500 = 0xFFff5722;
+    public static final int Brown_500 = 0xFF795548;
+    public static final int Grey_500 = 0xFF9e9e9e;
+    public static final int BlueGrey_500 = 0xFF607d8b;
+
+}
